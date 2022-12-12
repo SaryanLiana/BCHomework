049e3ae09a39ce00896b7055428c43adb56d3ed9
SaryanLiana <saryanliana6@gmail.com>
2022-12-12 12:12:30 +0400

    Shareholder contract

--- Shareholders.sol
+++ Shareholders.sol
@@ -3,11 +3,11 @@
 pragma solidity ^0.8;
 
 /**
-* @title Shareholders
+* @title Shareholder
 * @dev this contract adds and removes users by the owner and sends users
 * ether according to their percentage for not keeping ether in the contract 
 */
-contract Shareholders {
+contract Shareholder {
 
     /**
     * @dev Throws if called by any account other than the owner.
@@ -20,7 +20,7 @@ contract Shareholders {
     address owner;
     mapping(address => uint256) public userToPercent;
     address payable[] public users;
-    uint256 public total = 0;
+    uint256 public total;
 
     /**
     * The constructor initializes the owner.
@@ -33,7 +33,7 @@ contract Shareholders {
     * @dev sends ethers to users corresponding to their percentage.
     */
     receive() external payable {
-        for(uint256 _index = 0; _index < users.length - 1; _index++) {
+        for(uint256 _index; _index < users.length; _index++) {
             users[_index].transfer((address(this).balance * userToPercent[users[_index]]) / 100);
         }
     }
@@ -44,18 +44,18 @@ contract Shareholders {
     * @param _userAddress the address of the user that should be added.
     * @param _percentage the percentage that the user should receive from the amount.
     */
-    function addUsers(address payable _userAddress, uint256 _percentage) payable public onlyOwner {
-        if(userToPercent[_userAddress] == 0 && total + _percentage < 100) {
+    function addUsers(address payable _userAddress, uint256 _percentage) public onlyOwner {
+        require(total + _percentage <= 100, "total greater than 100");
+        if(userToPercent[_userAddress] == 0) {
             users.push(_userAddress);
-            total += _percentage;
         } 
 
-        for(uint256 i; i < users.length - 1; i++) {
-            if(userToPercent[_userAddress] != 0 && users[i] == _userAddress && total + _percentage - userToPercent[_userAddress] < 100) {
-                total -= userToPercent[_userAddress];
-                total += _percentage;
-            }
+        if(userToPercent[_userAddress] > _percentage){
+            total -= (userToPercent[_userAddress] - _percentage);
+        } else {
+            total += (_percentage - userToPercent[_userAddress]);
         }
+ 
         userToPercent[_userAddress] = _percentage; 
     }
 
@@ -65,7 +65,7 @@ contract Shareholders {
     * @param _userAddress the address of the user that should be removed.
     */
     function removeUsers(address _userAddress) public onlyOwner {
-        for(uint256 _index = 0; _index < users.length - 1; _index++) {
+        for(uint256 _index; _index < users.length; _index++) {
             if(users[_index] == _userAddress) {
                 users[_index] = users[users.length - 1];
                 users.pop();
